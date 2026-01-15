67dfdfdbcb5fa8489d53e0be9e07b237557a8d1b
Kiran <skirankumars31@gmail.com>
2026-01-15 13:29:35 +0100

    platformatic kafka

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { KafkaJS } from '@confluentinc/kafka-javascript';
+import { Admin, Producer, stringSerializers, jsonSerializer} from '@platformatic/kafka'
 
 const bootstrapServer = 'localhost:9092';
 
@@ -25,7 +26,7 @@ test.skip('send message', async ({ page }) => {
   await producer.disconnect();
 });
 
-test('create topic and send message', async ({ page }) => {
+test.skip('confluent - create topic and send message', async ({ page }) => {
 
   const kafka = new KafkaJS.Kafka({
     kafkaJS: {
@@ -53,4 +54,42 @@ test('create topic and send message', async ({ page }) => {
   });
 
   await producer.disconnect();
+});
+
+test('platformatic - create topic and send message', async ({ page }) => {
+  // Create an admin client
+  const admin = new Admin({
+    clientId: 'my-admin',
+    bootstrapBrokers: [bootstrapServer]
+  })
+
+  // Create topics
+  await admin.createTopics({
+    topics: ["test-topic"],
+    partitions: 1,
+    replicas: 1
+  })
+
+// Close the admin client when done
+  await admin.close()
+
+  const producer = new Producer({
+    clientId: 'my-producer',
+    bootstrapBrokers: [bootstrapServer], // Replace with your Kafka broker(s)
+    serializers: stringSerializers,
+  });
+
+  // Send messages
+  await producer.send({
+    messages: [
+      {
+        topic: "test-topic",
+        key: "1",
+        value: 'Hello, Kafka!'
+      }
+    ]
+  });
+
+  await producer.close();
+
 });
